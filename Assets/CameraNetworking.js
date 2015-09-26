3b31c6de8a1b4b2a37b13359fca42306361fe377
vanstorm9 <antlowhur@yahoo.com>
2015-09-26 11:15:43 -0400

    Got rigid object to work with sword

---
+++ Assets/CameraNetworking.js
@@ -0,0 +1,11 @@
+﻿#pragma strict
+
+function Start (){
+ 
+     if(GetComponent.<NetworkView>().isMine){
+         GetComponent(Camera).enabled = true;
+     }
+     else{
+         GetComponent(Camera).enabled = false;
+     }
+ }
